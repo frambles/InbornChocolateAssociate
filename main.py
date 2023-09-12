a21d919c9c9e3d5178dbfffc02b891c708efcdfa
JakobRoach <18141144-JakobRoach@users.noreply.replit.com>
2023-09-12 18:49:41 +0000

    implemented a bad fibonacci number function

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ def fact(n):
   else:
     return 1
 
+def fib_bad(n):
+  if n > 1:
+    return fib_bad(n-1) + fib_bad(n-2)
+  else:
+    return 1
+
 def main():
   for i in range(20):
-    print(f"{i}:\t{fact(i)}")
+    print(f"{i}:\t{fib_bad(i)}")
 
 if __name__ == "__main__":
   main()
