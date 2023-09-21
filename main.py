bf4e249a98f1f8f1a758af918ccb41a3e68088a9
frambles <145711595+frambles@users.noreply.github.com>
2023-09-21 18:55:47 +0000

    timers for both functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
-
 from time import perf_counter
+
 
 def fact(n):
   if n > 0:
@@ -17,12 +17,19 @@ def fib_bad(n):
 def main():
   with open("fib_bad.csv", 'w', newline='') as csvfile: 
     csvwriter = csv.writer(csvfile)
-    for i in range(50):
-      exe_time = perf_counter()
+    for i in range(30):
+
+      print(f"running iteration {i}...")
+      fib_time = perf_counter()
       fib_val = fib_bad(i)
-      exe_time = perf_counter() - exe_time
+      fib_time = perf_counter() - fib_time
+      
+      fact_time = perf_counter()
+      fact_val = fact(i)
+      fact_time = perf_counter() - fact_time
+      
       #print(f"{i}, {fib_val}, {exe_time}")
-      csvwriter.writerow([i, fib_val, exe_time])
+      csvwriter.writerow([i, fib_val, fact_val, fib_time, fact_time])
 
 if __name__ == "__main__":
   main()
